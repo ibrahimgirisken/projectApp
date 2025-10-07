9baed351ced856621325cab7f5ac84ae7ecbc2ec
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-10-07 14:45:53 +0300

    2025-10-07-14-45

--- src/components/sections/heros/heroFour.tsx
+++ src/components/sections/heros/heroFour.tsx
@@ -6,31 +6,19 @@ import 'swiper/css/pagination'
 import 'swiper/css/effect-fade'
 import Link from "next/link"
 import { motion } from "motion/react"
-
-const heroSlides = [
-    {
-        image: '/img/hero/hero-7.jpg',
-        title: 'We Provide our clients Best Solar Services',
-        description: 'Transmds is the world’s driving worldwide coordinations supplier we uphold industry and exchange the worldwide trade of merchandi',
-        link: '/',
-    },
-    {
-        image: '/img/hero/hero-5.jpg',
-        title: 'We Provide our clients Best Solar Services',
-        description: 'Transmds is the world’s driving worldwide coordinations supplier we uphold industry and exchange the worldwide trade of merchandi',
-        link: '/',
-    },
-    {
-        image: '/img/hero/hero-9.jpg',
-        title: 'We Provide our clients Best Solar Services',
-        description: 'Transmds is the world’s driving worldwide coordinations supplier we uphold industry and exchange the worldwide trade of merchandi',
-        link: '/',
-    },
-]
-
-
+import { useBanner } from "@/features/banner/hooks/useBanner"
+import { useLocale } from "next-intl"
 
 const HeroFour = () => {
+    const { data: banners = [], isLoading, error } = useBanner();
+    const locale = useLocale();
+    if (isLoading) return <div>Menü yükleniyor…</div>;
+    if (error) return <div>Menü yüklenirken hata oluştu.</div>;
+    const resolveBannerSrc = (img?: string) => {
+  if (!img) return '';
+  if (img.startsWith('http')) return img;
+  return `/uploads/banners/${img}`;
+};
     return (
         <section className="hero-section hero-1">
             <div className="array-button">
@@ -56,64 +44,75 @@ const HeroFour = () => {
                 }}
                 modules={[Navigation, Pagination, EffectFade, Autoplay]}
             >
-                {heroSlides.map((slide, index) => (
-                    <SwiperSlide key={index}>
-                        {(({ isActive }) => (
-                            <>
-                                <div className="hero-image bg-cover" style={{ backgroundImage: `url(${slide.image})` }} />
-                                <div className="container">
-                                    <div className="row g-4">
-                                        <div className="col-lg-8">
-                                            <motion.div
-                                                initial={{ opacity: 0 }}
-                                                animate={{ opacity: isActive ? 1 : 0 }}
-                                                className="hero-content"
-                                            >
-                                                <motion.h1
-                                                    initial={{ y: 20, opacity: 0 }}
-                                                    whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
-                                                    transition={{
-                                                        duration: .5,
-                                                        delay: 0.3,
-                                                        ease: 'linear',
-                                                    }}
-                                                >
-                                                    {slide.title}
-                                                </motion.h1>
-                                                <motion.p
-                                                    initial={{ y: 20, opacity: 0 }}
-                                                    whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
-                                                    transition={{
-                                                        duration: .5,
-                                                        delay: 0.5,
-                                                        ease: 'linear',
-                                                    }}
-                                                    style={{ maxWidth: '820px' }}
-                                                >
-                                                    {slide.description}
-                                                </motion.p>
+                {banners.map((slide, index) => {
+                    const langData = slide.bannerTranslations.find((t) => t.langCode === locale);
+                    slide.bannerTranslations?.[0];
+                    return (
+                        <SwiperSlide key={index}>
+                            {(({ isActive }) => (
+                                <>
+                                    <div className="hero-image bg-cover" style={{ backgroundImage: `url(${resolveBannerSrc(slide.desktopImage)})` }} />
+                                    <div className="container">
+                                        <div className="row g-4">
+                                            <div className="col-lg-8">
                                                 <motion.div
-                                                    initial={{ y: 20, opacity: 0 }}
-                                                    whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
-                                                    transition={{
-                                                        duration: .5,
-                                                        delay: 0.7,
-                                                        ease: 'linear',
-                                                    }}
-                                                    className="hero-button"
+                                                    initial={{ opacity: 0 }}
+                                                    animate={{ opacity: isActive ? 1 : 0 }}
+                                                    className="hero-content"
                                                 >
-                                                    <Link href={slide.link} className="theme-btn theme-color-2" data-animation="slideUp" data-delay="1.7">
-                                                        <span>Learn More <i className="fas fa-chevron-right" /></span>
-                                                    </Link>
+                                                    <motion.h1
+                                                        initial={{ y: 20, opacity: 0 }}
+                                                        whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
+                                                        transition={{
+                                                            duration: .5,
+                                                            delay: 0.3,
+                                                            ease: 'linear',
+                                                        }}
+                                                    >
+                                                        {langData?.title}
+                                                    </motion.h1>
+                                                    <motion.p
+                                                        initial={{ y: 20, opacity: 0 }}
+                                                        whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
+                                                        transition={{
+                                                            duration: .5,
+                                                            delay: 0.5,
+                                                            ease: 'linear',
+                                                        }}
+                                                        style={{ maxWidth: '820px' }}
+                                                    >
+                                                        {langData?.content}
+                                                    </motion.p>
+                                                    <motion.div
+                                                        initial={{ y: 20, opacity: 0 }}
+                                                        whileInView={{ y: isActive ? '0' : 20, opacity: isActive ? 1 : 0 }}
+                                                        transition={{
+                                                            duration: .5,
+                                                            delay: 0.7,
+                                                            ease: 'linear',
+                                                        }}
+                                                        className="hero-button"
+                                                    >
+                                                        <Link
+                                                            href={langData?.url ?? "#"}
+                                                            className="theme-btn theme-color-2"
+                                                            data-animation="slideUp"
+                                                            data-delay="1.7"
+                                                        >
+                                                            <span>
+                                                                {langData?.title ?? "Learn More"} <i className="fas fa-chevron-right" />
+                                                            </span>      </Link>
+
+                                                    </motion.div>
                                                 </motion.div>
-                                            </motion.div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </>
-                        ))}
-                    </SwiperSlide>
-                ))}
+                                </>
+                            ))}
+                        </SwiperSlide>
+                    )
+                })}
             </Swiper>
         </section>
 
